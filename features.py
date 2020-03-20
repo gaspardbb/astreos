2e26cd5d1fccc9794ff0f3592264bd2443449f18
gaspardb <gaspard.beugnot@polytechnique.edu>
2020-03-20 12:40:00 +0100

    added all_features function.

--- features.py
+++ features.py
@@ -44,6 +44,48 @@ def first_diff(df: pd.DataFrame, var_level=None):
     return result
 
 
+@CF.validate
+def all_features(df: pd.DataFrame, get_first_diff=True):
+    """
+    Compute all the features from the input df.
+    All other functions implemented in this file should be added to this function!
+
+    Parameters
+    ----------
+    df:
+        The input DataFrame (compliant with CheckFeatures' formatting).
+    get_first_diff:
+        Whether to compute the first difference of all the computed features.
+
+    Returns
+    -------
+    A DataFrame with time as index, and columns having two levels:
+        * WF: [1, 2, 3, 4, 5, 6]
+        * var: [var_1, ..., var_n]
+    """
+    # Basic Features
+    temperature = mean_of_var(df, "T")
+    clouds = mean_of_var(df, "CLCT")
+    speed = wind_speed(df)
+    direction = wind_direction(df)
+    features = pd.concat([temperature, clouds, speed, direction], axis=1)
+
+    # Insert your feature function call here!
+
+    # Whether to compute first diff for all features
+    if get_first_diff:
+        diff = []
+        variables = features.columns.get_level_values("var").unique()
+        for var in variables:
+            var_diff = first_diff(features, var_level=var)
+            diff.append(var_diff)
+
+        features = pd.concat([features] + diff, axis=1)
+
+    return features
+
+
+
 def check_isin(value, accepted_values):
     if not value in accepted_values:
         raise ValueError(f"You did not pass a good value. Got: {value} when accepted values are: {accepted_values}.")
@@ -60,5 +102,4 @@ if __name__ == '__main__':
     df = load_train_data()
     a = wind_direction(df)
     a_diff = first_diff(a)
-    # r = wind_speed(df)
-    # t = mean_of_var(df, 'T')
+    features = all_features(df)
